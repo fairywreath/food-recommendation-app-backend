629e5d8b22af390779e921fc62b7190b5f72c461
B3JMIN <h34cai@uwaterloo.ca>
2024-02-27 20:58:18 -0500

    update api

--- server/app.py
+++ server/app.py
@@ -1,11 +1,21 @@
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv(override=True)
 
 app = FastAPI(debug=True)
+
+
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 from server.web_api import api
